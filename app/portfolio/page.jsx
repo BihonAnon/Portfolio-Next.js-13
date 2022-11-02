9be0df2aa62795567c9d9c108a9c3baf2fb4e9de
byron <byrondalberg@gmail.com>
2022-11-01 19:35:36 -0700

    did some stuff

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -2,6 +2,10 @@ export default function Portfolio() {
     return (
         <div className="portfolio">
             <h1>Portfolio</h1>
+            <div>Some stuff
+                <div>Some more stuff</div>
+
+            </div>
         </div>
     );
 }
